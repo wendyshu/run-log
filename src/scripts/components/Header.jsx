0f9878de3295d590aab8f17b1a068a1dcb287a64
Bryan Smith <bryanesmith@gmail.com>
2017-08-18 22:32:29 -0400

    navbar active link styling via react-router-bootstrap

--- src/scripts/components/Header.jsx
+++ src/scripts/components/Header.jsx
@@ -1,6 +1,8 @@
 /*eslint-disable no-unused-vars*/
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { IndexLinkContainer } from 'react-router-bootstrap';
+import { NavItem } from 'react-bootstrap';
 /*eslint-enable no-unused-vars*/
 
 export default () => (
@@ -10,8 +12,12 @@ export default () => (
         <span className="navbar-brand">Run Log</span>
       </div>
       <ul className="nav navbar-nav nav-pills">
-        <li role="presentation" className="active"><a href="/#/">Home</a></li>
-        <li role="presentation"><a href="/#/activity">Activity</a></li>
+        <IndexLinkContainer activeClassName="active" to="/">
+          <NavItem>Home</NavItem>
+        </IndexLinkContainer>
+        <IndexLinkContainer activeClassName="active" to="/activity">
+          <NavItem>Activity</NavItem>
+        </IndexLinkContainer>
       </ul>
     </div>
   </nav>
