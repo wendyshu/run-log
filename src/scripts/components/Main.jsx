55b6d4dfd7b07bf401e182a45e9d58d8d21ea021
Bryan Smith <bryanesmith@gmail.com>
2017-08-23 00:09:51 -0400

    Add todo for activity loader higher-order component

--- src/scripts/components/Main.jsx
+++ src/scripts/components/Main.jsx
@@ -5,6 +5,14 @@ import Dashboard from '../features/dashboard/Dashboard.jsx';
 import Activity from '../features/activity/Activity.jsx';
 /*eslint-enable no-unused-vars*/
 
+//
+// TODOs:
+//  1. convert to component, and
+//  2. load data from here instead of activity component so available to entire
+//     app. (state.events instead of state.activity.events)
+//  3. Show network indicator.
+//  4. Convert just this logic to ActivityLoader higher-order component.
+//
 export default () => (
   <div className="container">
     <Switch>
