ed3d2c2e010ae17eb3bf4ed30818d93f233e8e75
Bryan Smith <bryanesmith@gmail.com>
2017-08-27 18:40:50 -0400

    Extract DashboardBarChart components

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,10 +1,9 @@
 /*eslint-disable no-unused-vars*/
 import React from 'react';
-import ChartistGraph from 'react-chartist';
 
 import DashboardAggregateStats from '../dashboardWidgets/DashboardAggregateStats.jsx';
+import DashboardBarChart from '../dashboardWidgets/DashboardBarChart.jsx';
 import DashboardShoesStats from '../dashboardWidgets/DashboardShoesStats.jsx';
-import DashboardStats from '../dashboardWidgets/DashboardStats.jsx';
 import DashboardTabs from './DashboardTabs.jsx';
 import FeaturedRun from '../featuredRun/FeaturedRun.jsx';
 /*eslint-enable no-unused-vars*/
@@ -13,7 +12,6 @@ import moment from 'moment';
 
 import { TAB_7_DAY, TAB_30_DAY, TAB_365_DAY, TAB_ALL } from './constants';
 import { connect } from 'react-redux';
-import { add } from '../../scripts/utils/math'; // TODO: move
 
 class Dashboard extends React.Component {
 
@@ -57,73 +55,6 @@ class Dashboard extends React.Component {
       .filter(e => e['@type'] === 'ChangeShoes');
   }
 
-  // TODO: move
-  subtractMoment(fromMoment, units, duration) {
-    const args = {};
-    args[units] = duration;
-    return fromMoment.clone().subtract(args);
-  }
-
-  // TODO: move
-  momentSeries(length, units, duration) {
-    const dates = [];
-
-    for (let i = length - 1; i >= 0; i--) {
-      const date = this.subtractMoment(moment(), units, i * duration);
-      dates.push(date);
-    }
-
-    return dates;
-  }
-
-  // TODO: move
-  barChartDate(events, barOpts, dateLabelFn) {
-
-    const eventsMoments = events.map(e => Object.assign({}, e, { date: moment(e.date) }));
-    const dates = this.momentSeries(barOpts.count, barOpts.units, barOpts.length);
-
-    const series = dates.map(endDate => {
-      const startDate = this.subtractMoment(endDate, barOpts.units, barOpts.length);
-      return eventsMoments.filter(e => e.date.isBetween(startDate, endDate))
-        .map(e => e.distance)
-        .reduce(add, 0);
-    });
-
-    return {
-      labels: dates.map(dateLabelFn),
-      series: [
-        series
-      ]
-    };
-  }
-
-  // TODO: move
-  barChartOptions() {
-    return {
-      axisY: {
-        labelInterpolationFnc: function(value) {
-          return `${value} mi`;
-        }
-      }
-    };
-  }
-
-  // TODO: move
-  selectedTabBarChart(selectedTab, tabData) {
-    const chartEvents = this.eventsSince(tabData.chartStartMoment);
-
-    switch (selectedTab) {
-    case TAB_7_DAY:
-      return (<ChartistGraph data={this.barChartDate(chartEvents, { count: 7, units: 'Day', length: 1 }, m => m.format('dd'))} options={this.barChartOptions()} type={'Bar'} />);
-    case TAB_30_DAY:
-      return (<ChartistGraph data={this.barChartDate(chartEvents, { count: 10, units: 'Day', length: 3 }, m => m.format('MM/DD'))} options={this.barChartOptions()} type={'Bar'} />);
-    case TAB_365_DAY:
-      return (<ChartistGraph data={this.barChartDate(chartEvents, { count: 12, units: 'Month', length: 1 }, m => m.format('MMM'))} options={this.barChartOptions()} type={'Bar'} />);
-    case TAB_ALL:
-      return (<ChartistGraph data={this.barChartDate(chartEvents, { count: 8, units: 'Year', length: 1 }, m => m.format('YYYY'))} options={this.barChartOptions()} type={'Bar'} />);
-    }
-  }
-
   render() {
     const tabData = this.tabData();
     const periodEvents = this.eventsSince(tabData.periodStartMoment);
@@ -146,8 +77,10 @@ class Dashboard extends React.Component {
         <div className="row">
           <div className="col-md-6">
             <h2><div className="label label-info">Distance</div></h2>
-            {/* <div className="widget-stub col-xs-12">(chart)</div> */}
-            { this.selectedTabBarChart(this.props.dashboard.ui.selectedTab, tabData) }
+            <div className="dashboard-bar-chart">
+              <DashboardBarChart selectedTab={this.props.dashboard.ui.selectedTab}
+                events={this.eventsSince(tabData.chartStartMoment)}/>
+            </div>
           </div>
           <div className="col-md-6">
             <h2><div className="label label-info">{this.tabData().statsLabel}</div></h2>
---
+++ src/components/dashboardWidgets/DashboardBarChart.jsx
@@ -0,0 +1,111 @@
+/*eslint-disable no-unused-vars*/
+import React from 'react';
+import ChartistGraph from 'react-chartist';
+/*eslint-enable no-unused-vars*/
+
+import moment from 'moment';
+
+import { TAB_7_DAY, TAB_30_DAY, TAB_365_DAY, TAB_ALL } from '../dashboard/constants';
+import { add } from '../../scripts/utils/math';
+
+/*
+ * Given moment, subtracts time.
+ *
+ * E.g., fromMoment = moment("2017-01-21"), units = 'Week', duration = 2,
+ *   returns moment("2017-01-07").
+ */
+function subtractMoment(fromMoment, units, duration) {
+  const args = {};
+  args[units] = duration;
+  return fromMoment.clone().subtract(args);
+}
+
+/*
+ * Returns a series of moments ending in now with specified
+ *   length and durations.
+ *
+ * E.g., length = 5, units = 'Days', duration = 2,
+ *   returns [10, 8, 6, 4, 2, 0].map(d => moment().subtract({days: d}))
+ */
+function momentSeries(length, units, duration, endDate) {
+  const dates = [];
+
+  for (let i = length - 1; i >= 0; i--) {
+    const date = subtractMoment(endDate, units, i * duration);
+    dates.push(date);
+  }
+
+  return dates;
+}
+
+/*
+ * Generates data for bar chart.
+ */
+function barChartData(events, xLabelFn, barOpts) {
+
+  const eventsMoments = events.map(e => Object.assign({}, e, { date: moment(e.date) }));
+  const dates = momentSeries(barOpts.count, barOpts.units, barOpts.length, moment());
+
+  const series = dates.map(endDate => {
+    const startDate = subtractMoment(endDate, barOpts.units, barOpts.length);
+    return eventsMoments.filter(e => e.date.isBetween(startDate, endDate))
+      .map(e => e.distance)
+      .reduce(add, 0);
+  });
+
+  return {
+    labels: dates.map(xLabelFn),
+    series: [
+      series
+    ]
+  };
+}
+
+/*
+ * Common options for all bar charts.
+ */
+function barChartOptions() {
+  return {
+    axisY: {
+      labelInterpolationFnc: function(value) {
+        return `${value} mi`;
+      }
+    }
+  };
+}
+
+/*
+ * Given selected tabs, returns the parameters needed for barChartData.
+ */
+function selectedTabBarChartParams(selectedTab) {
+  switch (selectedTab) {
+  case TAB_7_DAY:
+    return {
+      barOpts: { count: 7, units: 'Day', length: 1 },
+      xLabelFn: m => m.format('dd')
+    };
+  case TAB_30_DAY:
+    return {
+      barOpts: { count: 10, units: 'Day', length: 3 },
+      xLabelFn: m => m.format('MM/DD')
+    };
+  case TAB_365_DAY:
+    return {
+      barOpts: { count: 12, units: 'Month', length: 1 },
+      xLabelFn: m => m.format('MMM')
+    };
+  case TAB_ALL:
+    return {
+      barOpts: { count: 8, units: 'Year', length: 1 },
+      xLabelFn: m => m.format('YYYY')
+    };
+  }
+}
+
+/*
+ * Renders a bar chart for 7d, 30d, 365d, or all-time.
+ */
+export default (props) => {
+  const params = selectedTabBarChartParams(props.selectedTab);
+  return (<ChartistGraph data={barChartData(props.events, params.xLabelFn, params.barOpts)} options={barChartOptions()} type={'Bar'} />);
+};
