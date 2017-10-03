65db87a4a0603fff73c0e2ae4b004ef3f624c963
Bryan Smith <bryanesmith@gmail.com>
2017-10-02 21:56:08 -0400

    AddEvent: reference impl of DropdownButton

--- src/components/activity/addEvent/AddEvent.jsx
+++ src/components/activity/addEvent/AddEvent.jsx
@@ -1,6 +1,6 @@
 /*eslint-disable no-unused-vars*/
 import React from 'react';
-import { Modal } from 'react-bootstrap';
+import { DropdownButton, MenuItem, Modal } from 'react-bootstrap';
 import { showModal, hideModal } from './actions';
 /*eslint-enable no-unused-vars*/
 
@@ -12,15 +12,24 @@ class AddEvent extends React.Component {
     super(props);
   }
 
+  eventTypeSelected(eventKey, event) {
+    console.log(`DEBUG: ${eventKey}`, event);
+  }
+
   render() {
     return (
       <div>
         <Modal show={this.props.addEvent.ui.showModal} onHide={this.props.hideModal}>
           <Modal.Header closeButton>
-            <Modal.Title>Add Entry</Modal.Title>
+            <Modal.Title>Add entry</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <p>Spoon</p>
+            <DropdownButton title="Select entry type" id="select-event-type" onSelect={this.eventTypeSelected}>
+              <MenuItem eventKey="1">Run</MenuItem>
+              <MenuItem eventKey="2">Cross-Train</MenuItem>
+              <MenuItem eventKey="3">Run + Cross-Train</MenuItem>
+              <MenuItem eventKey="4">Change Running Shoes</MenuItem>
+            </DropdownButton>
           </Modal.Body>
         </Modal>
         <button onClick={this.props.showModal} type="button" className="btn btn-primary btn-lg">
