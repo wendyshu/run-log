3936d23f88d8286c6537d0dc6c7af32f01405d38
Bryan Smith <bryanesmith@gmail.com>
2017-10-06 21:46:18 -0400

    vendor codesplitting w/ cache busting

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,18 @@ const extractCSS = new ExtractTextPlugin('app.bundle.css');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const LIBS = [ 'bootstrap', 'chartist', 'es6-promise', 'isomorphic-fetch', 'jquery', 'moment', 'moment-duration-format', 'react', 'react-bootstrap', 'react-chartist', 'react-dom', 'react-loader', 'react-redux', 'react-router-bootstrap', 'react-router-dom', 'redux', 'redux-logger', 'redux-thunk' ];
+
 module.exports = {
   context: path.resolve(__dirname, '.'),
-  entry: './src/scripts/app.js',
+  entry: {
+    bundle: './src/scripts/app.js',
+    vendor: LIBS
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'app.bundle.js'
+    filename: '[name].[chunkhash].bundle.js'
   },
   module: {
     rules: [
@@ -66,6 +71,9 @@ module.exports = {
     new CleanWebpackPlugin([ 'dist' ], { verbose: true }),
     new HtmlWebpackPlugin({
       template: 'src/index.html'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
     })
   ]
 };
