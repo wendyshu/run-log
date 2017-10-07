76d275fe43e71d4eb674d933a60ab5d571d33014
Bryan Smith <bryanesmith@gmail.com>
2017-10-06 21:36:42 -0400

    using html-webpack-plugin to load js bundle

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const extractCSS = new ExtractTextPlugin('app.bundle.css');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   context: path.resolve(__dirname, '.'),
@@ -64,8 +64,8 @@ module.exports = {
       'window.Tether': 'tether',
     }),
     new CleanWebpackPlugin([ 'dist' ], { verbose: true }),
-    new CopyWebpackPlugin([
-      { from: 'index.html' }
-    ])
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
   ]
 };
