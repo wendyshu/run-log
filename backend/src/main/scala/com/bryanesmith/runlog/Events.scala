9e58271cf5d9288a79c0cc53775d6776d5bfa47f
Bryan Smith <bryanesmith@gmail.com>
2017-12-29 14:29:31 -0500

    Category, Type enumerations

--- backend/src/main/scala/com/bryanesmith/runlog/Events.scala
+++ backend/src/main/scala/com/bryanesmith/runlog/Events.scala
@@ -1,6 +1,8 @@
 package com.bryanesmith.runlog
 
-import io.circe.Json
+import com.bryanesmith.runlog.Events.Category.Category
+import io.circe.Decoder.Result
+import io.circe.{Decoder, Encoder, HCursor, Json}
 import io.circe.generic.auto._
 import io.circe.generic.extras._
 import io.circe.literal._
@@ -29,6 +31,38 @@ object Events {
       "events" -> Json.arr(events.map(_.asJson): _*)
     )
 
+  object Category extends Enumeration {
+    type Category = Value
+    val Casual: Value   = Value("casual")
+    val Speed: Value    = Value("speed")
+    val Distance: Value = Value("distance")
+    val Race: Value     = Value("race")
+  }
+
+  implicit val categoryDecoder: Decoder[Category.Value] = new Decoder[Events.Category.Value] {
+    override def apply(c: HCursor): Result[Events.Category.Value] = Decoder.decodeString.map(Category.withName).apply(c)
+  }
+
+  implicit val categoryEncoder: Encoder[Category.Value] = new Encoder[Events.Category.Value] {
+    override def apply(a: Events.Category.Value): Json = Encoder.encodeString.apply(a.toString)
+  }
+
+  object Type extends Enumeration {
+    type Type = Value
+    val RunCrossTrain: Value  = Value("Run+CrossTrain")
+    val Run: Value            = Value
+    val CrossTrain: Value     = Value
+    val ChangeShoes: Value    = Value
+  }
+
+  implicit val typeDecoder: Decoder[Type.Value] = new Decoder[Events.Type.Value] {
+    override def apply(c: HCursor): Result[Events.Type.Value] = Decoder.decodeString.map(Events.Type.withName).apply(c)
+  }
+
+  implicit val TypeEncoder: Encoder[Type.Value] = new Encoder[Events.Type.Value] {
+    override def apply(a: Events.Type.Value): Json = Encoder.encodeString.apply(a.toString)
+  }
+
   //
   // TODO: Event sealed trait with inheritance.
   //
@@ -40,9 +74,9 @@ object Events {
   @ConfiguredJsonCodec
   case class Event (
     atId: String,
-    atType: String, // TODO: enum
+    atType: Type.Value,
     date: String,
-    category: Option[String] = None, // TODO: enum
+    category: Option[Category.Value] = None,
     distance: Option[Double] = None,
     duration: Option[String] = None,
     notes: Option[String] = None,
