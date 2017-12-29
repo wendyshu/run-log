d11e7919621a0ef9deb90fb75e97f5c6999ad635
Bryan Smith <bryanesmith@gmail.com>
2017-12-29 12:48:45 -0500

    json w/ spaces

--- backend/src/main/scala/com/bryanesmith/runlog/JSON.scala
+++ backend/src/main/scala/com/bryanesmith/runlog/JSON.scala
@@ -4,7 +4,7 @@ import io.circe.{Json, Printer}
 
 object JSON {
 
-  private val printer = Printer.noSpaces.copy(dropNullKeys = true)
+  private val printer = Printer.spaces2.copy(dropNullKeys = true)
 
   def format(json:Json): String = printer.pretty(json)
 }
