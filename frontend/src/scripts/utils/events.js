a392651294fa2977496b5cc93613b29c1f3ea30d
Bryan Smith <bryanesmith@gmail.com>
2018-02-11 19:36:26 -0500

    add intervals support to average pace

--- frontend/src/scripts/utils/events.js
+++ frontend/src/scripts/utils/events.js
@@ -1,3 +1,4 @@
+import { durationToSeconds } from './dates';
 import { add } from './math';
 
 /**
@@ -6,8 +7,11 @@ import { add } from './math';
 export function distance(event) {
   if (event.run && event.run.distance) {
     return event.run.distance;
-  } else if (event.run && event.run.totalDistance) {
-    return event.run.totalDistance;
+  } else if (
+    event.run && event.run.count && event.run.intervalDuration && event.run.intervalSpeed
+  ) {
+    const secs = durationToSeconds(event.run.intervalDuration);
+    return event.run.count * secs * event.run.intervalSpeed / 3600;
   } else {
     return 0;
   }
@@ -19,4 +23,38 @@ export function distance(event) {
  */
 export function totalDistance(events) {
   return events.map(distance).reduce(add, 0);
+}
+
+/**
+ * Returns the pace for event in seconds per mile. If pace not available, returns 0.
+ */
+export function pace(event) {
+  if (event.run && event.run.distance && event.run.duration) {
+    return durationToSeconds(event.run.duration) / event.run.distance;
+  } else if (event.run && event.run.intervalSpeed) {
+    return 3600 / event.run.intervalSpeed;
+  } else {
+    return 0;
+  }
+}
+
+/**
+ * Returns the seconds for an event.
+ */
+export function seconds(event) {
+  if (event.run && event.run.duration) {
+    return durationToSeconds(event.run.duration);
+  } else if (event.run && event.run.count && event.run.intervalDuration) {
+    const secs = durationToSeconds(event.run.intervalDuration);
+    return event.run.count * secs;
+  } else {
+    return 0;
+  }
+}
+
+/**
+ * Returns the average pace for a bunch of events.
+ */
+export function averagePace(events) {
+  return events.map(seconds).reduce(add, 0) / totalDistance(events);
 }
--- frontend/src/scripts/utils/events.spec.ts
+++ frontend/src/scripts/utils/events.spec.ts
@@ -1,26 +1,63 @@
-import { distance, totalDistance } from './events';
+import { averagePace, distance, pace, seconds, totalDistance } from './events';
+
+const nonRun = {};
+const steadyRun = {run: {distance: 6, duration: "PT1H"}};
+const intervals = {run: {count: 6, intervalDuration: "PT1M", intervalSpeed: 10.0}};
 
 test('distance for non-running event', () => {
-  expect(distance({})).toBe(0);
+  expect(distance(nonRun)).toBe(0);
 });
 
 test('distance for steady-state run', () => {
-  expect(distance({run: {distance: 5}})).toBe(5);
+  expect(distance(steadyRun)).toBe(6);
 });
 
 test('distance for intervals', () => {
-  expect(distance({run: {totalDistance: 2.2}})).toBe(2.2);
+  expect(distance(intervals)).toBe(1);
 });
 
 test('totalDistance for 0 events', () => {
-  expect(distance([])).toBe(0);
+  expect(totalDistance([])).toBe(0);
 });
 
-test('totalDistance for 0 events', () => {
+test('totalDistance for several types of events', () => {
   const events = [
-    {},
-    {run: {distance: 5}},
-    {run: {totalDistance: 2.2}}
+    nonRun,
+    steadyRun,
+    intervals
   ];
-  expect(distance(events)).toBe(0);
+  expect(totalDistance(events)).toBe(7);
+});
+
+test('pace for non-running event', () => {
+  expect(pace(nonRun)).toBe(0);
+});
+
+test('pace for steady-state run', () => {
+  expect(pace(steadyRun)).toBe(600);
+});
+
+test('pace for intervals', () => {
+  expect(pace(intervals)).toBe(360);
+});
+
+test('seconds for non-running event', () => {
+  expect(seconds(nonRun)).toBe(0);
+});
+
+test('seconds for steady-state run', () => {
+  expect(seconds(steadyRun)).toBe(3600);
+});
+
+test('seconds for intervals', () => {
+  expect(seconds(intervals)).toBe(360);
+});
+
+test('averagePace for several types of events', () => {
+  const events = [
+    nonRun,
+    steadyRun,
+    intervals
+  ];
+  expect(averagePace(events)).toBe(565.7142857142857);
 });
