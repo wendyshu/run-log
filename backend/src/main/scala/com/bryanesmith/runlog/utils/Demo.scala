6ee9a0a0f9d6c1c661172a93219c971387b6bae5
Bryan Smith <bryanesmith@gmail.com>
2018-04-17 21:00:40 -0400

    log today's exercise

--- backend/src/main/scala/com/bryanesmith/runlog/utils/Demo.scala
+++ backend/src/main/scala/com/bryanesmith/runlog/utils/Demo.scala
@@ -8,6 +8,12 @@ object Demo {
 
   def events = Seq(
     Event(
+      atId = "_:n121",
+      atType = Type.CrossTrain,
+      date = "2018-04-17",
+      notes = Some("Press, rows, DL, standing tricep extensions.")
+    ),
+    Event(
       atId = "_:n120",
       atType = Type.CrossTrain,
       date = "2018-04-10",
